4206e8a9925b2854bbce000f026b103b9229e3ef
Ruuffyyy <yasin.ariseven@gmail.com>
2026-04-28 21:36:39 +0200

    verbessert i vergessen bei der kommunikation pi zu ESP und test

--- ESP32TEST.py
+++ ESP32TEST.py
@@ -1,8 +1,33 @@
 import serial
 import time
 
-ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
-time.sleep(2)
-ser.write(b"Hallo vom Raspberry Pi\n")
-ser.write(b"USB Stick erkannt\n")
-ser.close()
+try:
+    ser = serial.Serial(
+        "/dev/ttyACM0",
+        115200,
+        timeout=1,
+        write_timeout=2,
+        xonxoff=False,
+        rtscts=False,
+        dsrdtr=False
+    )
+
+    time.sleep(3)
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
+
+    ser.write(b"Hallo vom Raspberry Pi\n")
+    ser.flush()
+    time.sleep(0.5)
+
+    ser.write(b"USB Stick erkannt\n")
+    ser.flush()
+    time.sleep(0.5)
+
+    print("Senden erfolgreich")
+    ser.close()
+
+except serial.SerialTimeoutException:
+    print("Schreiben in /dev/ttyACM0 hat Timeout -> ESP antwortet/akzeptiert nicht sauber")
+except serial.SerialException as e:
+    print("Serial-Fehler:", e)
